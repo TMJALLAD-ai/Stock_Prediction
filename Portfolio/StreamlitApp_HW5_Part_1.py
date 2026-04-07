ce7024765cdce5eed6927b1d05bd63cf02e05b22
TMJALLAD-ai <T.M.JALLAD@tcu.edu>
2026-04-07 17:47:36 -0500

    Refactor imports and improve code structure

--- Portfolio/StreamlitApp_HW5_Part_1.py
+++ Portfolio/StreamlitApp_HW5_Part_1.py
@@ -114,7 +114,7 @@ def display_explanation(input_df, session, aws_bucket):
     explainer_name = MODEL_INFO["explainer"]
     explainer = load_shap_explainer(session, aws_bucket, posixpath.join('explainer', explainer_name),os.path.join(tempfile.gettempdir(), explainer_name))
     
-    dataset = pd.read_csv(r'./SP500Data.csv',index_col=0)
+    dataset = pd.read_csv(os.path.join(current_dir, 'SP500Data.csv'), index_col=0)
     random = 'IBM'
     random_price = user_inputs['IBM']
     closest_date = (dataset[random] - float(random_price)).abs().idxmin()
